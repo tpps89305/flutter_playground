943fb0ae15d82092ac4a56036dfec4bfe8b965e1
Dispy-Mac <tpps89305@hotmail.com>
2022-02-22 14:19:17 +0800

    Add more buttons

--- lib/button_page.dart
+++ lib/button_page.dart
@@ -42,6 +42,37 @@ class ButtonPage extends StatelessWidget {
     );
   }
 
+  IconButton _buildIconButton(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          Fluttertoast.showToast(
+              msg: "按下了 IconButton",
+              backgroundColor: Theme.of(context).primaryColorDark);
+        },
+        icon: const Icon(Icons.add));
+  }
+
+  FloatingActionButton _buildFloatingActionButton(BuildContext context) {
+    return FloatingActionButton(
+        child: const Icon(Icons.save),
+        onPressed: () {
+          Fluttertoast.showToast(
+              msg: "按下了 FloatingActionButton",
+              backgroundColor: Theme.of(context).primaryColorDark);
+        });
+  }
+
+  ElevatedButton _buildElevatedButtonWithIcon(BuildContext context) {
+    return ElevatedButton.icon(
+        onPressed: () {
+          Fluttertoast.showToast(
+              msg: "按下了 ElevatedButtonWithIcon",
+              backgroundColor: Theme.of(context).primaryColorDark);
+        },
+        icon: const Icon(Icons.access_alarm),
+        label: const Text("With Icon"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,7 +83,10 @@ class ButtonPage extends StatelessWidget {
           children: [
             _buildTextButton(context),
             _buildOutlinedButton(context),
-            _buildElevatedButton(context)
+            _buildElevatedButton(context),
+            _buildIconButton(context),
+            _buildFloatingActionButton(context),
+            _buildElevatedButtonWithIcon(context)
           ],
         ),
       ),
