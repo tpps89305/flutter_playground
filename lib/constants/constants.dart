f5051c8eb7c94d3c6e214d3a0c11fd560749bb86
Dispy-Desktop <tpps89305@hotmail.com>
2023-03-26 00:54:13 +0800

    Edit Button, SnackBar demo.

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -5,8 +5,7 @@ import 'navigation_routes.dart';
 class Constants {
   static List<ContentItem> get contentItems {
     List<ContentItem> list = [];
-    list.add(ContentItem("按鈕", route: NavigationRoutes.buttonPage));
-    list.add(ContentItem("SnackBar", route: NavigationRoutes.snackPage));
+    list.add(ContentItem("按鈕、SnackBar", route: NavigationRoutes.buttonPage));
     list.add(ContentItem("選單按鈕", route: NavigationRoutes.popupMenuButtonPage));
     list.add(ContentItem("下拉式選單", route: NavigationRoutes.dropdownButtonPage));
     list.add(ContentItem("文字輸入", route: NavigationRoutes.textFieldPage));
